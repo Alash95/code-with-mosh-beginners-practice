4604fbd0d498386d2b96ebf2f72a590f219caf0d
Alash95 <o.alasho205@gmail.com>
2024-11-24 12:06:46 +0100

    Hello World in c#

--- _01_Hello_World/Program.cs
+++ _01_Hello_World/Program.cs
@@ -9,5 +9,6 @@ class Program
         Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
         Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
         Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+       
     }
 }
